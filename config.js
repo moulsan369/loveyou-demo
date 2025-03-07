fd2eeef750632cf4b6fc752c5b609677c08785fb
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2025-03-07 23:31:35 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'H ơi, chúc mừng 8/3 nè!',
+    introDesc: 'Hôm nay là ngày của em, anh có điều muốn hỏi em, trả lời thật lòng nhé!',
+    btnIntro: 'Dạ oke anh ơi',
+    title: 'Em có yêu anh không nè? 😘',
+    desc: 'Nếu em không trả lời hoặc thoát ra, tức là em đồng ý làm vợ anh đó nha :>',
+    btnYes: 'Yêu anh lắm luôn á <33',
+    btnNo: 'Hổng có đâu mà :333',
+    question: 'Nói anh nghe lý do vì sao em yêu anh đi nào <3',
+    btnReply: 'Gửi cho anh nè <3',
+    reply: 'Vì anh vừa đẹp trai vừa yêu thương em hihi <333',
+    mess: 'Anh biết mà, em là cô gái tuyệt vời nhất! 🥰 Yêu em 8/3 và mãi mãi nha 😘',
+    messDesc: 'Giờ thì nhắn tin cho anh đi, anh chờ em đó!',
+    btnAccept: 'Dạ liền luôn <3',
+    messLink: 'http://fb.com' 
+}
